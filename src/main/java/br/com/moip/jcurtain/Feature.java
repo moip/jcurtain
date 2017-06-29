e38a555a49acb3d469877aaa7411cc76e72535aa
Eduardo Colabardini <eduardocolabardini@gmail.com>
2017-06-29 00:46:09 -0300

    add and fix tests

--- src/main/java/br/com/moip/jcurtain/Feature.java
+++ src/main/java/br/com/moip/jcurtain/Feature.java
@@ -3,6 +3,7 @@ package br.com.moip.jcurtain;
 import java.util.Set;
 
 public class Feature {
+
     private final String name;
     private final int percentage;
     private final Set<String> users;
@@ -24,4 +25,17 @@ public class Feature {
     public Set<String> getUsers() {
         return users;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Feature feature = (Feature) o;
+
+        if (percentage != feature.percentage) return false;
+        if (name != null ? !name.equals(feature.name) : feature.name != null) return false;
+        return users != null ? users.equals(feature.users) : feature.users == null;
+    }
+
 }
--- src/test/java/br/com/moip/jcurtain/JCurtainTest.java
+++ src/test/java/br/com/moip/jcurtain/JCurtainTest.java
@@ -5,12 +5,11 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.internal.util.collections.Sets;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertFalse;
@@ -24,15 +23,12 @@ public class JCurtainTest {
     @Mock
     private Jedis jedis;
 
-    private Set<String> testSet;
-
     private JCurtain jCurtain;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
         jCurtain = new JCurtain(jedisPool);
-        testSet = null;
 
         Mockito.when(jedisPool.getResource()).thenReturn(jedis);
     }
@@ -40,7 +36,6 @@ public class JCurtainTest {
     @Test
     public void returnsTrueOnOneHundredPercent() {
         Mockito.when(jedis.get("feature:name:percentage")).thenReturn("100");
-        Mockito.when(jedis.smembers("feature:name:users")).thenReturn(testSet);
 
         assertTrue(jCurtain.isOpen("name"));
     }
@@ -48,7 +43,6 @@ public class JCurtainTest {
     @Test
     public void returnsFalseOnZeroPercent() {
         Mockito.when(jedis.get("feature:name:percentage")).thenReturn("0");
-        Mockito.when(jedis.smembers("feature:name:users")).thenReturn(testSet);
 
         assertFalse(jCurtain.isOpen("name"));
     }
@@ -56,37 +50,32 @@ public class JCurtainTest {
     @Test
     public void returnsTrueOnListedUser() throws Exception {
         JCurtain jcurtain = new JCurtain(jedisPool);
-
-        Mockito.when(jedis.get("feature:name:percentage")).thenReturn("0");
         Mockito.when(jedis.sismember("feature:name:users", "test-user")).thenReturn(true);
 
         assertTrue(jcurtain.isOpen("name", "test-user"));
     }
 
+    @Test
+    public void returnTrueOnUnlistedUserButOneHundredPercent() {
+        Mockito.when(jedis.get("feature:name:percentage")).thenReturn("100");
+        Mockito.when(jedis.sismember("feature:name:users", "test-user")).thenReturn(false);
+
+        assertTrue(jCurtain.isOpen("name", "test-user"));
+    }
 
     @Test
     public void returnsFalseOnUnlistedUser() {
-        Set<String> testSet = new HashSet<String>(Arrays.asList("test-user"));
-
         Mockito.when(jedis.get("feature:name:percentage")).thenReturn("0");
-        Mockito.when(jedis.smembers("feature:name:users")).thenReturn(testSet);
+        Mockito.when(jedis.sismember("feature:name:users", "test-user")).thenReturn(true);
 
         assertFalse(jCurtain.isOpen("name", "test-invalid-user"));
     }
-
 
     @Test
     public void returnsFalseOnNullPercentage() {
         Mockito.when(jedis.get("feature:name:percentage")).thenReturn(null);
 
         assertFalse(jCurtain.isOpen("name"));
-    }
-
-    @Test
-    public void returnsFalseOnNullUsers() {
-        Mockito.when(jedis.get("feature:name:users")).thenReturn(null);
-
-        assertFalse(jCurtain.isOpen("name", "user-teste"));
     }
 
     @Test
@@ -99,6 +88,15 @@ public class JCurtainTest {
     @Test
     public void returnsFalseOnFailedConnection() {
         assertFalse(jCurtain.isOpen("name"));
+    }
+
+    @Test
+    public void shouldReturnFeature() {
+        Set<String> members = Sets.newSet("u1", "u2");
+        Mockito.when(jedis.get("feature:name:percentage")).thenReturn("50");
+        Mockito.when(jedis.smembers("feature:name:users")).thenReturn(members);
+
+        assertTrue(jCurtain.getFeature("name").equals(new Feature("name", 50, members)));
     }
 
 }
